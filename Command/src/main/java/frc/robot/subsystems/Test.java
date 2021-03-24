773b864ace0125b5d7b6e0a4c08c9a3918db2225
shadows198 <ryanbennett102@gmail.com>
2021-03-24 17:34:40 -0400

    h2

---
+++ Command/src/main/java/frc/robot/subsystems/Test.java
@@ -0,0 +1,17 @@
+
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Test extends SubsystemBase {
+  /**
+   * Creates a new ExampleSubsystem.
+   */
+  public Test() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
